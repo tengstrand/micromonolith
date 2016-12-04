0643033fcbfaadbe99105b9e94c488da0d8f7dc7
Joakim Tengstrand <joakimtengstrand@gmail.com>
2016-12-04 12:50:47 +0100

    Added symlinks to all pom.xml files.

--- java/development/setup.sh
+++ java/development/setup.sh
@@ -7,9 +7,18 @@
 # the project can be structured and how new directories can be added.
 
 mkdir src
-mkdir test
+mkdir pom-files
 
-cd src
+cd pom-files
+ln -s ../../address/pom.xml address-pom.xml
+ln -s ../../addressApi/pom.xml addressApi-pom.xml
+ln -s ../../email/pom.xml email-pom.xml
+ln -s ../../emailApi/pom.xml emailApi-pom.xml
+ln -s ../../rest/pom.xml test-pom.xml
+ln -s ../../test-data-generator/pom.xml test-data-generator-pom.xml
+ln -s ../../user/pom.xml user-pom.xml
+
+cd ../src
 mkdir -p main/java/micromonolith
 mkdir -p test/java/micromonolith
 
