c0fa03d71574252d85ed0f97143af5c98fbd3a14
Joakim Tengstrand <joakimtengstrand@gmail.com>
2016-12-04 15:00:36 +0100

    Initial commit.

---
+++ java/address/src/main/java/micromonolith/address/Address.java
@@ -0,0 +1,33 @@
+package micromonolith.address;
+
+import micromonolith.address_api.AddressData;
+
+public class Address implements AddressData {
+    private int streetNumber;
+    private String streetName;
+    private String zipcode;
+    private String city;
+
+    public Address(int streetNumber, String streetName, String zipcode, String city) {
+        this.streetName = streetName;
+        this.streetNumber = streetNumber;
+        this.zipcode = zipcode;
+        this.city = city;
+    }
+
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
