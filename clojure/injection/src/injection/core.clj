24a653c7f974617721b1d2e0c443c6a04524d17d
Joakim Tengstrand <joakimtengstrand@gmail.com>
2016-12-15 23:16:30 +0100

    Initial commit.

--- clojure/injection/src/injection/core.clj
+++ clojure/injection/src/injection/core.clj
@@ -3,9 +3,7 @@
 (defmacro inject [to from]
   "Replaces a function with another functions definition.
    to: the function that is going to be replaced (should include the namespace)
-   from: the function that is copied
-
-   Author: Joakim Tengstrand"
+   from: the function that is copied"
   (let [to-ns (symbol (namespace to))
         to-ns-symbol (symbol (name to))]
     (list 'intern
