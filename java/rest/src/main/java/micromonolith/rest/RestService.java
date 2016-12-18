6312dc69f1999aa7fd17d59702eb4d8090e2e2b7
Joakim Tengstrand <joakimtengstrand@gmail.com>
2016-12-18 20:11:14 +0100

    Initial commit.

--- java/rest/src/main/java/micromonolith/rest/RestService.java
+++ java/rest/src/main/java/micromonolith/rest/RestService.java
@@ -10,35 +10,36 @@ import java.util.List;
 
 /**
  * This is our REST API that we expose.
- * It manages the dependency injection for services that is calling
+ * It manages the dependency injection for services that are calling
  * other services.
  *
  * The idea is to show the principles of the monolith architecture,
  * so the code is free from technical stuff like HTTP and database connections,
- * that is up to you to imagine in your mind!
+ * that is up to your imagination!
  */
 public class RestService {
 
-    // Here we instantiates all our stateless services.
+    // Here we instantiate all our stateless services.
     private AddressService addressService = new AddressService();
     private EmailService emailService = new EmailService();
     private UserService userService = new UserService();
 
     // We inject one "function" at a time (*).
-    // Every "functions" is just a Java interfaces with only one method,
+    // Every "function" is just a Java interface with only one method,
     // but this is the closest we get to functions in Java!
     //
     // (*) We introduce the term "micro injection".
-    //     By using smaller building blocks ("functions" instead of interfaces
-    //     containing many methods) we get a more robust and flexible
+    //     By using smaller building blocks (for example "functions"
+    //     instead of interfaces containing many methods)
+    //     we get a more robust and flexible
     //     interface between services with a minimum of exposure.
     {
         userService.pdfSender = emailService;
     }
 
     /**
-     * The Address and AddressCriteria is part of the addressApi project,
-     * and is used to integrate this service with the address service.
+     * The Address and AddressCriteria are part of the addressApi project,
+     * and are used to integrate this service with the address service.
      */
     public void findAddresses() {
         AddressCriteria criteria = new AddressCriteria();
